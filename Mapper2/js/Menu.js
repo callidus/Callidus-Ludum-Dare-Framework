65254b8aaa78b42763746959871c8b96b6eb7932
Tim Kelsey <tim.callidus@gmail.com>
2011-12-07 12:44:02 +0000

    - work on menu panels - added working google ads

--- Mapper2/js/Menu.js
+++ Mapper2/js/Menu.js
@@ -1,7 +1,7 @@
 
 function toggleObjVisible(obj)
 {
-// 	obj.style.visibility = ( obj.style.visibility == 'visible' ) ? 'hidden' : 'visible';
+ 	obj.style.visibility = ( obj.style.visibility == 'visible' ) ? 'hidden' : 'visible';
 	obj.style.display = ( obj.style.display == 'block' ) ? 'none' : 'block';
 }
 
@@ -13,8 +13,8 @@ function toggleMenu( id, btn )
 	if( lastMenu != id )
 	{
 		btn.setAttribute( "class", "menu_item selected" );
-		//obj = document.getElementById( id );
-		//toggleObjVisible( obj );
+		obj = document.getElementById( id );
+		toggleObjVisible( obj );
 		
 		if( lastMenu == null )
 		{
@@ -29,8 +29,8 @@ function toggleMenu( id, btn )
 			
 			lastBtn = btn;
 			
-			//old = document.getElementById( lastPopup );
-			//toggleObjVisible( old );
+			old = document.getElementById( lastMenu );
+			toggleObjVisible( old );
 			lastMenu = id;
 		}
 	}
@@ -42,6 +42,6 @@ function initMenu( id, btn )
 	lastBtn.setAttribute( "class", "menu_item selected" );
 	
 	lastMenu = id;
-	//obj = document.getElementById( id );
-	//toggleObjVisible( obj );
+	obj = document.getElementById( id );
+	toggleObjVisible( obj );
 }
