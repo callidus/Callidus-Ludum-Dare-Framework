a1e99dc3f635dddf6556a11c8e0f244a8931f9b6
Tim Kelsey <callidus@titan.(none)>
2011-12-10 15:11:57 +0000

    - re-working the UI, added popup menus

--- Mapper2/js/Body.js
+++ Mapper2/js/Body.js
@@ -7,8 +7,38 @@ function resize()
 	var canvasMain = document.getElementById( "canvas_main" ); 
 
 	// 90%
-	windowheight = ( windowheight / 100 ) * 90;	
+	windowheight = ( windowheight / 100 ) * 95;	
 	frame.style.height = windowheight + "px"; 
-	canvasLeft.style.height = windowheight - 84 + "px";
+	//canvasLeft.style.height = windowheight - 84 + "px";
 	canvasMain.style.height = windowheight - 84 + "px";
 }
+
+var menuOpen = false;
+function loadTileSheet( menu, form )
+{
+	if( !menuOpen )
+	{
+		menuOpen = true;
+		try 
+		{			
+			menuRoot = document.getElementById( menu );
+			formRoot = document.getElementById( form );
+			if( menuRoot && formRoot )
+			{
+				menuRoot.style.visibility = 'visible';
+				menuRoot.style.display = 'block';
+				
+				formRoot.elements['create'].onclick = function( e ) {
+					menuRoot.style.visibility = 'hidden';
+					menuRoot.style.display = 'none';
+					menuOpen = false;
+				};
+			}
+		}
+		catch( err ) 
+		{
+			menuOpen = false;
+		}
+	}
+}
+
--- Mapper2/js/Menu.js
+++ Mapper2/js/Menu.js
@@ -45,3 +45,72 @@ function initMenu( id, btn )
 	obj = document.getElementById( id );
 	toggleObjVisible( obj );
 }
+
+// - dragging popups ---------------------------------------------------
+var dragObj = new Object();
+dragObj.zIndex = 0;
+
+function dragStart( event, id ) 
+{
+	var el;
+	var x, y;
+
+	if (id)
+	{
+		dragObj.elNode = document.getElementById( id );
+	}
+	else 
+	{
+		dragObj.elNode = event.target;
+
+		// If this is a text node, use its parent element.
+		if (dragObj.elNode.nodeType == 3)
+		{
+			dragObj.elNode = dragObj.elNode.parentNode;
+		}
+	}
+
+	// Get cursor position with respect to the page.
+	x = event.clientX + window.scrollX;
+	y = event.clientY + window.scrollY;
+
+	dragObj.cursorStartX = x;
+	dragObj.cursorStartY = y;
+	dragObj.elStartLeft  = parseInt(dragObj.elNode.style.left, 10);
+	dragObj.elStartTop   = parseInt(dragObj.elNode.style.top,  10);
+
+	if (isNaN(dragObj.elStartLeft)) dragObj.elStartLeft = 0;
+	if (isNaN(dragObj.elStartTop))  dragObj.elStartTop  = 0;
+
+	// Update element's z-index.
+
+	dragObj.elNode.style.zIndex = ++dragObj.zIndex;
+
+	// Capture mousemove and mouseup events on the page.
+	document.addEventListener( "mousemove", dragGo,   true);
+	document.addEventListener( "mouseup",   dragStop, true);
+	event.preventDefault();
+}
+
+function dragGo(event) 
+{
+	var x, y;
+	
+	// Get cursor position with respect to the page.
+	x = event.clientX + window.scrollX;
+	y = event.clientY + window.scrollY;
+
+	// Move drag element by the same amount the cursor has moved.
+	dragObj.elNode.style.left = (dragObj.elStartLeft + x - dragObj.cursorStartX) + "px";
+	dragObj.elNode.style.top  = (dragObj.elStartTop  + y - dragObj.cursorStartY) + "px";
+	event.preventDefault();
+}
+
+function dragStop(event) 
+{
+	// Stop capturing mousemove and mouseup events.
+	document.removeEventListener( "mousemove", dragGo,   true );
+	document.removeEventListener( "mouseup",   dragStop, true );
+}
+// ---------------------------------------------------------------------
+
