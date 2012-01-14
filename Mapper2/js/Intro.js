a209689918b458741bbd016bd2ac5ae9fc828386
Tim Kelsey <callidus@titan.(none)>
2012-01-14 12:10:23 +0000

    - updated intro info

--- Mapper2/js/Intro.js
+++ Mapper2/js/Intro.js
@@ -9,14 +9,15 @@ please see licence.txt
 function showIntro()
 {
 	showInfo("info_menu", "info_form", 
-"Welcome to Mapper v0.2 BETA\
----------------------------\
-This software is beta and still in active development. \
-It is (c) Tim Kelsey 2011 and released under the MIT License\
-----------------------------\
-v0.2 First public release.\
-.\
-.\
-.\
-" );
+"Welcome to Mapper v0.3 BETA\n\
+---------------------------\n\
+This software is beta and still in active development.\
+It is (c) Tim Kelsey 2012 and released under the MIT License\n\
+----------------------------\n\
+v0.3 Second Public Release.\n\
+- Fixed several UI bugs\n\
+- Added status pannel and tile info\n\
+- Added default tiles for demo\n\n\
+v0.2 First Public Release.\n"
+	);
 }
